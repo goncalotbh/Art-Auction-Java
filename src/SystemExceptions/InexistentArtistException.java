07b9b06bd5f9907b135d0ab445972830a91136d6
Gonçalo Domingues de Oliveira <152343759+goncalotbh@users.noreply.github.com>
2024-10-16 17:29:53 +0100

    Update InexistentArtistException.java

--- src/SystemExceptions/InexistentArtistException.java
+++ src/SystemExceptions/InexistentArtistException.java
@@ -3,7 +3,6 @@ package SystemExceptions;
 import java.io.Serializable;
 
 /**
- * @author Afonso Godinho 65153
  * @author Gonçalo Oliveira 65549
  *
  * This exception is thrown when the given login exists in the system but does not belong to an artist
