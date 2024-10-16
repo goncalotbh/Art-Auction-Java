99b3760c1ccf8120032fbaa78072b259f2428074
Gonçalo Domingues de Oliveira <152343759+goncalotbh@users.noreply.github.com>
2024-10-16 17:27:52 +0100

    Update BidBelowMinException.java

--- src/SystemExceptions/BidBelowMinException.java
+++ src/SystemExceptions/BidBelowMinException.java
@@ -3,7 +3,6 @@ package SystemExceptions;
 import java.io.Serializable;
 
 /**
- * @author Afonso Godinho 65153
  * @author Gonçalo Oliveira 65549
  *
  * This exception is thrown when the value of the bid made is lower than the minimum value accepted
