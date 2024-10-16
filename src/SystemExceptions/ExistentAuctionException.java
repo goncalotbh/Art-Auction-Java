abc51549c51322f4158631e9fd1fd009b79b3661
Gonçalo Domingues de Oliveira <152343759+goncalotbh@users.noreply.github.com>
2024-10-16 17:28:45 +0100

    Update ExistentAuctionException.java

--- src/SystemExceptions/ExistentAuctionException.java
+++ src/SystemExceptions/ExistentAuctionException.java
@@ -3,7 +3,6 @@ package SystemExceptions;
 import java.io.Serializable;
 
 /**
- * @author Afonso Godinho 65153
  * @author Gonçalo Oliveira 65549
  *
  * This exception is thrown when there is already an existent auction with the given identifier
