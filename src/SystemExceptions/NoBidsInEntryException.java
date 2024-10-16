f16c7c5ba23127dff0ea055d01ea0183d198bdb4
Gonçalo Domingues de Oliveira <152343759+goncalotbh@users.noreply.github.com>
2024-10-16 17:32:08 +0100

    Update NoBidsInEntryException.java

--- src/SystemExceptions/NoBidsInEntryException.java
+++ src/SystemExceptions/NoBidsInEntryException.java
@@ -3,7 +3,6 @@ package SystemExceptions;
 import java.io.Serializable;
 
 /**
- * @author Afonso Godinho 65153
  * @author Gonçalo Oliveira 65549
  *
  * This exception is thrown when there are no bids for the given work inside the auction
